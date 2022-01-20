83b7a3dcd360b42d7b6fe95cb0e8da9fbef55ed3
Tomer <kadison.tomer@gmail.commy>
2022-01-20 19:46:24 +0200

    forgot to upload

--- DataBase/src/main/java/Main.java
+++ DataBase/src/main/java/Main.java
@@ -7,11 +7,8 @@ import Data.TableHelpers.TableShower;
 public class Main {
     public static void main(String[] args) {
         DataBase dataBase = new DataBase("dataDir");
-        Table table = dataBase.getTable("people");
-        for (int i =1;i<=90;i++){
-            table.insert(new String[]{ String.valueOf(i),String.valueOf(i),"23/04/1973"} );
-        }
-        table.addNewHashIndex("age");
-        table.findByIndex("age","18");
+        dataBase.getTable("people").alter("1","name","0");
+        new TableSearcher(dataBase.getTable("people")).search("name","=","9");
     }
+
 }
